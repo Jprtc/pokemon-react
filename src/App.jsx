45382de725eea9c67fc1ede7dc5a8d6f0b45e62a
Joao Pedro de Rossi <jprtc8@gmail.com>
2026-06-16 21:18:18 -0300

    Implementando GTM no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Pokemon from "./Pages/Pokemon";
+import AnalyticsTracker from "./Components/AnalyticsTracker/AnalyticsTracker";
 
 
 function App() {
   return (
     <BrowserRouter>
+    <AnalyticsTracker/>
       <Routes>
         <Route path="/pokemon-react/" element={<Home/>}     /> 
           <Route
           path="/pokemon-react/:pokemonId"
           element={<Pokemon />}
         />
-
       </Routes>
     </BrowserRouter>
   )
--- src/Components/PokemonCard/PokemonCard.jsx
+++ src/Components/PokemonCard/PokemonCard.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom"
 
 function PokemonCard({nome,caminhoImagem,estagio, evoluirPokemon, id }){
+
+    const registrarClick = (destino) =>{
+        console.log('Evento de clique enviado, destino:',destino)
+        window.dataLayer.push({
+            event:"click",
+            destino:destino,
+        })
+    }
+
     return (
          <div className='CardPokemon'>
-            <Link to={`/pokemon-react/${id}`}>
+            <Link to={`/pokemon-react/${id}`} onClick={() => registrarClick(nome)} >
                 <img src={caminhoImagem} width={260} height={280} alt={`Ir para a página do ${nome}`}  loading="lazy"    />
             </Link>
             <h2>{nome}</h2>
             <p>Estágio: {estagio}</p>
-            <button aria-label={`Evoluir o Pokemon ${nome}`} onClick={()=> evoluirPokemon(nome)}>Evoluir</button>
+            <button aria-label={`Evoluir o Pokemon ${nome}`} onClick={()=> {evoluirPokemon(nome); registrarClick(`Evoluir ${nome}`)}}>Evoluir</button>
           </div>
     )
 }
